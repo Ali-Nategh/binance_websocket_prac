85b65bae37fdbad64b8927ed246719bca9196fd2
Ali Nategh <alinategh005@gmail.com>
2023-02-03 16:10:57 +0330

    add everything to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'
+import { SocketModule } from './websocket/websocket.module';
+import { BinanceSocketModule } from './test_binance_socket/binance.module';
+import { RedisCacheModule } from './test_redis_cache/redis-cache.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/ws'),
+    SocketModule,
+    // BinanceSocketModule,
+    // RedisCacheModule,
+    // ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
